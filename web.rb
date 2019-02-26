e6b0a80c7639145788ddfe0769a196e4e1ce9fa6
Astrid Corena <astrid.corena@gmail.com>
2019-02-26 12:39:53 -0500

    first commit

---
+++ web.rb
@@ -0,0 +1,25 @@
+
+require 'sinatra'
+require 'make_todo'
+
+
+get '/' do
+  @notes = Tarea.all
+  @title = 'Planning your day'
+  erb :index
+end
+
+post '/' do
+  Tarea.create(params[:content].capitalize)
+  redirect '/'
+end
+
+get '/:id/delete' do
+  Tarea.destroy(params[:id])
+  redirect '/'
+end
+
+get '/:id/complete' do
+  Tarea.update(params[:id])
+  redirect '/'
+end
